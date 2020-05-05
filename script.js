0a81726ba61f254ba0705a29a27d41243b710648
rajat641 <singhrajat641@gmail.com>
2020-05-05 03:46:06 -0700

    Day-2 Operator Precendence/Associativity

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ console.log(job);
 
 /*****
 Varible mutation and type coercion
-**/
+
 
 var firstName = 'John';
 var age = 28;
@@ -42,6 +42,82 @@ alert(firstName+'is a'+ age+
 // ask user an imput
 var lastName =prompt('Whats his name ??');
 console.log(firstName+"-"+lastName);
+
+*/
+
+/***************************
+Basic Operators
+
+//Math = +/-
+
+//Basic Math Operators
+var year = 2018
+var yearJohn = year - 28;
+var yearMark = year - 33
+
+console.log(yearJohn);
+var now  = 2020
+
+console.log(now+ 2)
+console.log(now*2)
+console.log(now/10)
+
+// Logical Operators
+
+var ageJohn = 28;
+var johnolder = ageJohn> 30;
+console.log(johnolder);
+
+//typeof operator
+
+console.log(typeof johnolder)
+console.log( typeof ageJohn)
+
+var x;
+console.log(typeof x); // undefined type
+
+*/
+
+/**
+Operator Precendence
+**/
+
+var now = 2018;
+var yearJohn = 1988;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge; // +- has higher precendemce that >= operator
+// we have 3 operators now : =, - , >=
+console.log(isFullAge);
+
+// Multiple Assignment
+
+var x, y;
+x = y = (3+7)*2; // Assignment works from R-> L
+console.log(x, y); // console for 2 variables
+
+// More operator
+
+x*=2 //x=  x*2
+console.log(x)
+
+x+=10
+console.log(x)
+
+x+=1 // x++
+console.log(x)
+x--; // x-=1
+console.log(x)
+
+
+
+
+
+
+
+
+
+
 
 
 
