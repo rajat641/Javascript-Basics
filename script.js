fc73d974fb1d66e305de19eb6b999e69c6092484
rajat641 <singhrajat641@gmail.com>
2020-05-05 03:10:13 -0700

    Day : 2-variable mutation/variable coercion/ User input

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/*
+Varibles and data types
+
+
 var firstName = "John";
 console.log(firstName);
 
@@ -10,3 +14,37 @@ console.log(fullAge);
 
 var job;
 console.log(job);
+
+*/
+
+/*****
+Varible mutation and type coercion
+**/
+
+var firstName = 'John';
+var age = 28;
+
+console.log("1"+age);
+
+var job , isMarried;
+job ='teacher';
+isMarried = false;
+
+console.log(firstName+'is a'+ age+ 
+	'year old'+ job+'.'+ 'Is he'+ isMarried);
+
+// var mutation
+ age = 'twenty eight'; // no need to define it again like var age etc
+
+
+alert(firstName+'is a'+ age+ 
+	'year old'+ job+'.'+ 'Is he'+ isMarried);
+// ask user an imput
+var lastName =prompt('Whats his name ??');
+console.log(firstName+"-"+lastName);
+
+
+
+
+
+
