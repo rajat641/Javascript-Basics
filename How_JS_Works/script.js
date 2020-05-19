ade100b63973b388fc0fbbcffa9478f4c743b35c
rajat641 <singhrajat641@gmail.com>
2020-05-19 04:25:07 -0700

    Scope Chain/ This Keyword

--- How_JS_Works/script.js
+++ How_JS_Works/script.js
@@ -26,3 +26,37 @@ var retirement = function(year){
 
 console.log(age) // output undefined as they are set to 0
 var age = 23;
+
+///////////// Lecture: The this keyword///// IMP
+//console.log(this)
+calage(133);
+
+function calage(year){
+	console.log(1999-year)
+	console.log(this) // regular function so here this keyword is attached to a global window object
+}
+
+var john = {
+	name: 'john',
+	yearOfBirth:1998,
+	calJohnAge: function(){
+		console.log(this) // this points to john object
+		// function inn(){
+		// 	console.log(this) // this here is window object as its not a method. Its a function written inside a method
+		// }
+		// inn()
+
+	}
+}
+//john.calJohnAge(334);
+
+
+var mike = {
+	name: 'Mike'
+};
+
+// use metho borrowing
+
+mike.calAge = john.calJohnAge;
+mike.calAge(132) // this now points to mike object.This demonstrate:"this" varible is not assigned a value until
+// a function where it is actually defined is called.
