cb31e1c764c23f3ee89ad7fd3c3ba7a0c9969e7f
rajat641 <singhrajat641@gmail.com>
2020-05-24 21:05:57 -0700

    Game Done-Week-3

--- DOM-game/app.js
+++ DOM-game/app.js
@@ -2,20 +2,48 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- In each turn, a player rolls a dice as many times as he whishes. 
+Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. 
+After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to 
+is GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
 
 
-var score, roundScore, activePlayer;
+var score, roundScore, activePlayer ;
 
-score = [0, 0];
-roundScore = 0;
-activePlayer= 0;
+var gamePlaying = true; // maintains sttate og the game
+// As we use  DRY principle
+function init(){
+	scores = [0, 0];
+	roundScore = 0;
+	activePlayer= 0;
+	document.getElementById('score-0').textContent  = '0'
+	document.getElementById('score-1').textContent  = '0'
+	document.getElementById('current-0').textContent  = '0'
+	document.getElementById('current-1').textContent  = '0'
 
+	document.querySelector('.dice').style.display = 'none';
+
+	document.getElementById('name-0').textContent = 'Player-1';
+	document.getElementById('name-1').textContent = 'Player-2';
+
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	// to be sure that there is no active class left
+
+	document.querySelector('.player-0-panel').classList.add('active'); 
+	//document.querySelector('.player-1-panel').classList.remove('active');
+
+
+}
+
+init();
 
 //dice = Math.floor(Math.random()*6) + 1 // gives a number between 0 and 1
  //console.log(dice)
@@ -34,11 +62,11 @@ var x = document.querySelector('#score-0').textContent;
 console.log(x)
 
 
-// set all inital score to 0
-document.getElementById('score-0').textContent  = '0'
-document.getElementById('score-1').textContent  = '0'
-document.getElementById('current-0').textContent  = '0'
-document.getElementById('current-1').textContent  = '0'
+// // set all inital score to 0
+// document.getElementById('score-0').textContent  = '0'
+// document.getElementById('score-1').textContent  = '0'
+// document.getElementById('current-0').textContent  = '0'
+// document.getElementById('current-1').textContent  = '0'
 
 // we can use query selector to change CSS
 // hide dice at beginning so we set display property to None
@@ -48,7 +76,7 @@ document.getElementById('current-1').textContent  = '0'
 //CSS
 // property:value
 // here we change CSS through query seelctor
-document.querySelector('.dice').style.display = 'none';
+// document.querySelector('.dice').style.display = 'none';
 
 // events handler 
 // on button roll
@@ -57,7 +85,9 @@ document.querySelector('.dice').style.display = 'none';
 //document.querySelector('.btn-roll').addEventListener('click', btn);
 document.querySelector('.btn-roll').addEventListener('click', function(){
 
-  //1. Random Number
+
+ if(gamePlaying){
+ 	//1. Random Number
   var dice = Math.floor(Math.random()*6) + 1 // we can't access varibles in scope of other functions
 
   //2. Display the result
@@ -76,7 +106,87 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
   }else{
   	// next player turn
-  	activePlayer = activePlayer == 1 ? 0:1; // ternary operator
+  	// activePlayer = activePlayer == 1 ? 0:1; // ternary operator
+  	// roundScore = 0; // reset round score
+
+
+  	// document.getElementById('current-0').textContent = '0';
+  	// document.getElementById('current-1').textContent = '0';
+  	// // as soon as player changes, we add active class to new player and remove it from previous player
+  	// // we can add.remove classes in Javascript to style it
+  	// // only works if active player is 1
+  	// //document.querySelector('.player-0-panel').classList.remove('active');
+  	// //document.querySelector('.player-1-panel').classList.add('active')
+  	
+
+  	// // use toggle i.e add a class if not present or remove if already present
+  	// document.querySelector('.player-0-panel').classList.toggle('active');
+  	// document.querySelector('.player-1-panel').classList.toggle('active');
+
+  	// // hide dice if player score is 1
+  	// document.querySelector('.dice').style.display = 'None';
+  	nextPlayer() // after applying DRY principle
+
+  }
+}
+
+  
+
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+		
+		if(gamePlaying){
+		// Add Current score to global score
+		scores[activePlayer] +=roundScore;
+
+		// Update the UI
+		document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer]
+
+
+		// Check if the player won the game // copied from above
+	// Apply DRY here
+	// activePlayer = activePlayer == 1 ? 0:1; // ternary operator
+ //  	roundScore = 0; // reset round score
+
+
+ //  	document.getElementById('current-0').textContent = '0';
+ //  	document.getElementById('current-1').textContent = '0';
+ //  	// as soon as player changes, we add active class to new player and remove it from previous player
+ //  	// we can add.remove classes in Javascript to style it
+ //  	// only works if active player is 1
+ //  	//document.querySelector('.player-0-panel').classList.remove('active');
+ //  	//document.querySelector('.player-1-panel').classList.add('active')
+  	
+
+ //  	// use toggle i.e add a class if not present or remove if already present
+ //  	document.querySelector('.player-0-panel').classList.toggle('active');
+ //  	document.querySelector('.player-1-panel').classList.toggle('active');
+
+ //  	// hide dice if player score is 1
+ //  	document.querySelector('.dice').style.display = 'None';
+
+ // if player won
+	 if(scores[activePlayer] >= 20){
+	 		// document.querySelector('#name-'+activePlayer).textContent = 'Winner!!';
+	 		document.getElementById('name-'+activePlayer).textContent = 'Winner!!';
+	 		document.querySelector('.dice').style.display = 'None';
+	 		document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+	 		document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');	
+	 		gamePlaying = false;
+	 	}
+	else{
+
+	 	// Next Player
+	 	nextPlayer();
+	 }
+}
+});
+
+
+function nextPlayer(){
+	activePlayer = activePlayer == 1 ? 0:1; // ternary operator
   	roundScore = 0; // reset round score
 
 
@@ -91,12 +201,10 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
   	// use toggle i.e add a class if not present or remove if already present
   	document.querySelector('.player-0-panel').classList.toggle('active');
-  	document.querySelector('.player-1-panel').classList.toggle('active')
+  	document.querySelector('.player-1-panel').classList.toggle('active');
 
   	// hide dice if player score is 1
   	document.querySelector('.dice').style.display = 'None';
+}
 
-
-  }
-
-});
+document.querySelector('.btn-new').addEventListener('click', init);
